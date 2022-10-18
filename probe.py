cfb4cbf4ebf4120fb0d56caffd76a002f58f1d07
televnaya <televnaya@mail.ru>
2022-10-18 23:38:44 +0300

    выполнено 1 д/з task_01.py

---
+++ probe.py
@@ -0,0 +1,11 @@
+# Домашнее задание
+
+import string
+
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+string_length = len(my_favorite_songs)
+print(my_favorite_songs[0:13])
+print(my_favorite_songs[-13:])
+print(my_favorite_songs[16:30])
+print(my_favorite_songs[-26:-15])
