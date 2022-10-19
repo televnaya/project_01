1257321aa4ea93b7cf6699fa98b43109bb494973
televnaya <televnaya@mail.ru>
2022-10-19 12:10:57 +0300

    исправлена ошибка task_01.py

--- task_01.py
+++ task_01.py
@@ -5,3 +5,10 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+string_length = len(my_favorite_songs)
+print(my_favorite_songs[0:14])
+print(my_favorite_songs[-13:])
+print(my_favorite_songs[16:30])
+print(my_favorite_songs[-26:-15])
