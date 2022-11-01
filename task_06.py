0268fb81d9cc1b8e93bd79ce524a0896efb54631
televnaya <televnaya@mail.ru>
2022-11-01 23:59:52 +0300

    выполнено д/з task_06.py

--- task_06.py
+++ task_06.py
@@ -15,3 +15,8 @@ my_favorite_songs = {
     'Nowhere to Run': 2.58,
     'In This World': 4.02,
 }
+import random
+songs_lenght = [3.03, 4.02, 3.40, 3.03, 5.28, 4.15, 4.04, 2.58, 4.02]
+random_lenght = random.sample (songs_lenght, 3)
+lengths_counted = sum(random_lenght)
+print(lengths_counted)
